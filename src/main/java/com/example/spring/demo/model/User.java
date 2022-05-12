dc44107eb6b4198a7168773a9af12cc928306457
aleksey.i.efremov <aleksey.i.efremov@rt.ru>
2022-05-12 20:36:25 +0300

    new commit

--- src/main/java/com/example/spring/demo/model/User.java
+++ src/main/java/com/example/spring/demo/model/User.java
@@ -5,6 +5,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import java.util.Collection;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -147,5 +148,18 @@ public class User implements UserDetails {
 
     public Set<Role> getRoles() {
         return roles;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
