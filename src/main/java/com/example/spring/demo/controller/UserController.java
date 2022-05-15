d4f9d4dd82cf3413e4a8176a033128105da42e3b
aleksey.i.efremov <aleksey.i.efremov@rt.ru>
2022-05-15 16:50:19 +0300

    new commit

--- src/main/java/com/example/spring/demo/controller/UserController.java
+++ src/main/java/com/example/spring/demo/controller/UserController.java
@@ -5,10 +5,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.security.Principal;
 
 @Controller
+@RequestMapping("/")
 public class UserController {
     private final UserService userService;
 
@@ -17,8 +19,12 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping(value = "/login")
+    @GetMapping
     public String getLoginPage() {
+        return "login";
+    }
+    @GetMapping("/login")
+    public String getHomePage() {
         return "login";
     }
 
